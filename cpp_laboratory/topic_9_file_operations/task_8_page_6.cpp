b4f454c7e1629a636241034f3765247cc6de4da3
y9938 <932810d3@gmail.com>
2024-04-04 14:41:10 +0300

    update task_8 topic 9

--- cpp_laboratory/topic_9_file_operations/task_8_page_6.cpp
+++ cpp_laboratory/topic_9_file_operations/task_8_page_6.cpp
@@ -6,30 +6,47 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+int word_line(string line);
 
 int main()
 {
     ifstream input_file("../8_input.txt");
 
     string line;
+    int max_word = 0; // Максимальное количество слов в строке
+    string max_line; // Строка с максимальным количеством слов
     while (getline(input_file, line))
     {
-        int count = 0;
-        int i = 0;
-        int len = line.length();
-        while (i < len)
+        int count_word = word_line(line);
+        if (count_word > max_word)
         {
-            while (line[i] != ' ')
-                ++i;
-            int begin = i;
-            while (line[i] == ' ')
-                ++i;
-            string word;
-            word = line.substr(begin, i - begin);
-            ++count;
+            max_word = count_word;
+            max_line = line;
         }
-        
     }
+
+    cout << "Max word: " << max_word << endl;
+    cout << "Max line: " << max_line << endl;
+}
+
+int word_line(string line)
+{
+    int count_word = 0;
+    int i = 0;
+    int len = line.length();
+    while (i < len)
+    {
+        while (line[i] == ' ')
+            ++i;
+        int begin = i;
+        while (line[i] != ' ' && i < len)
+            ++i;
+        string word;
+        word = line.substr(begin, i - begin);
+        if (word.length() > 0)
+            ++count_word;
+    }
+    return count_word;
 }
