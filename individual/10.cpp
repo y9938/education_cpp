b671c2eba6191954338a29a0f505a2dc00e8dfe9
y9938 <932810d3@gmail.com>
2024-04-24 00:28:33 +0300

    Update docs, add PDFs for individual tasks topics, and complete individual tasks 9 and 10

--- individual/10.cpp
+++ individual/10.cpp
@@ -2,6 +2,7 @@
 Создать файл с полями: фамилия, сумма долга,
 номер ЖЭКа. Вывести список должников.
 Определить сумму задолженностей по каждому ЖЭКу.
+P.s Тут я использую номера ЖЭКа в виде слов
 */
 
 #include <iostream>
@@ -9,30 +10,59 @@
 #include <string>
 using namespace std;
 
-const int MAX_HCS = 3;
+const int MAX_REC = 100;
 
 struct record
 {
     string surname; // Фамилия
     double debt_amount; // Сумма долга
-    int hcs_number; // Номер ЖЭКа
+    string hcs_number; // Номер ЖЭКа
 };
+
+void sort(record arr[], int size);
+void calculate_debts(record arr[], int size);
 
 int main()
 {
     ifstream in_f("../io_files/10_in.txt");
 
-    record rec;
-    double debt_hcs[MAX_HCS] = {}; // Суммы долгов по каждому ЖЭКу
-    while (in_f >> rec.surname >> rec.debt_amount >> rec.hcs_number)
-    {
-        if (rec.debt_amount > 0)
-            cout << rec.surname << "\t" << rec.debt_amount << "\t" << rec.hcs_number << endl;
+    record rec[MAX_REC];
+    int n_rec = 0; // Реальное количество записей
 
-        if (rec.hcs_number >= 1 && rec.hcs_number <= MAX_HCS)
-            debt_hcs[rec.hcs_number - 1] += rec.debt_amount;
+    while (in_f >> rec[n_rec].surname >> rec[n_rec].debt_amount >> rec[n_rec].hcs_number)
+    {
+        if (rec[n_rec].debt_amount > 0)
+            cout << rec[n_rec].surname << "\t" << rec[n_rec].debt_amount << "\t" << rec[n_rec].hcs_number << endl;
+        ++n_rec;
     }
-    cout << "Сумма по каждому ЖЭКу:\n";
-    for (int i = 0; i < MAX_HCS; ++i)
-        cout << i + 1 << ": " << debt_hcs[i] << endl;
+
+    sort(rec, n_rec);
+    calculate_debts(rec, n_rec);
+}
+
+void sort(record arr[], int size)
+{
+    for (int i = size - 1; i > 0; --i) 
+        for (int j = 0; j < i; ++j) 
+            if (arr[j].hcs_number > arr[j+1].hcs_number) 
+            {
+                record tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+}
+
+void calculate_debts(record arr[], int size)
+{
+    int i = 0;
+    cout << "\n" << "НОМЕР ЖЭКа" << "\tСУММА ДОЛГА" << "\n\n";
+    while (i < size) { // проход по массиву
+        string current_hcs = arr[i].hcs_number;
+        double total_debt = 0;
+        while (i < size && arr[i].hcs_number == current_hcs) {
+            total_debt += arr[i].debt_amount; // подсчет суммы долга по текущему ЖЭКу
+            ++i;
+        }
+        cout << current_hcs << "\t\t" << total_debt << "\n";
+    }
 }
--- individual/9.cpp
+++ individual/9.cpp
@@ -10,13 +10,14 @@
 #include <cctype>
 using namespace std;
 
-bool is_positive_int(const string& word)
+bool is_int(const string& word)
 {
     if (word.empty()) return false;
-    for (char ch : word)
-    {
-        if (!isdigit(ch)) return false;
-    }
+    int len = word.length();
+    int start = (word[0] == '-') ? 1 : 0;
+    if (len == start) return false; // Строка только `-`
+    for (int i = start; i < len; ++i) 
+        if (!isdigit(word[i])) return false;
     return true;
 }
 
@@ -32,19 +33,24 @@ int main()
         int len = line.length();
         while (i < len)
         {
-            while (i < len && (line[i] == ' ' || ispunct(line[i]))) ++i;
+            while (i < len && (line[i] == ' ' || (ispunct(line[i]) && line[i] != '-')))
+            {
+                out_f << line[i]; // Пробелы и пунктуация, кроме минуса
+                ++i;
+            }
             int begin = i;
+            if (i < len && line[i] == '-') ++i; // Проверяем наличие минуса перед числом
             while (i < len && (line[i] != ' ' && !ispunct(line[i]))) ++i;
             string word;
             word = line.substr(begin, i - begin);
-            if (is_positive_int(word))
+            if (is_int(word))
             {
-                int number = stoi(word);
-                number++;
-                out_f << number << " ";
+                int number = stoi(word); // Строку в число
+                ++number;
+                out_f << number;
             }
             else
-                out_f << word << " ";
+                out_f << word;
         }
         out_f << endl;
     }
