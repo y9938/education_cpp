cc55f1b960924fb339210d4c17aac97359bdff3a
y9938 <932810d3@gmail.com>
2024-10-10 20:14:35 +0300

    Update 14.cpp (individual)

--- individual/14.cpp
+++ individual/14.cpp
@@ -18,8 +18,8 @@ void add_node(List *&head, char input, List *&last_node)
 {
     List *new_node = new List; // Создаем новый узел
     new_node->sym = input;     // Устанавливаем символ
-    new_node->prev = 0;        // Указатель на предыдущий узел
-    new_node->next = 0;        // Указатель на следующий узел
+    new_node->prev = nullptr;  // Указатель на предыдущий узел
+    new_node->next = nullptr;  // Указатель на следующий узел
 
     if (!head)
     {
@@ -34,42 +34,70 @@ void add_node(List *&head, char input, List *&last_node)
     }
 }
 
-void exclude_words_by_char_code_sum(List *&head)
+void delete_node(List *&head, List *&node)
 {
-    List *temp = head;
-    while (temp)
+    if (node == head)
     {
-        List *start_word, *end_word;
-        if (temp->sym == ' ')
+        head = head->next;
+        if (head)
+            head->prev = nullptr;
+        delete node;
+    }
+    else
+    {
+        if (node->prev)
+            node->prev->next = node->next;
+        if (node->next)
+            node->next->prev = node->prev;
+        delete node;
+    }
+}
+
+void exclude_words_by_char_code_sum(List *&head, int number)
+{
+    List *current = head;
+
+    while (current)
+    {
+        List *start_word = nullptr, *end_word = nullptr;
+        int sum = 0;
+
+        while (current && current->sym == ' ')
         {
-            while (true)
+            current = current->next;
+        }
+        start_word = current;
+
+        while (current && current->sym != ' ')
+        {
+            sum += current->sym;
+            end_word = current;
+            current = current->next;
+        }
+
+        if (sum % number == 0 && start_word && end_word)
+        {
+            List *to_delete = start_word;
+            while (to_delete != end_word->next)
             {
-                if (temp->next->sym != ' ' && temp->next)
-                    break;
-                else
-                    temp = temp->next;
+                List *next_node = to_delete->next;
+                delete_node(head, to_delete);
+                to_delete = next_node;
             }
         }
-        start_word = temp->next;
-        while (temp->sym != ' ')
-        {
-            temp = temp->next;
-        }
-        end_word = temp->prev;
 
-        cout << "start word = " << start_word->sym << endl;
-        cout << "end_word = " << end_word->sym << endl;
-        /*temp = temp->next;*/
+        if (current)
+            current = current->next;
     }
 }
 
 void print_list(List *head)
 {
-    List *temp = head;
-    while (temp)
+    List *current = head;
+    while (current)
     {
-        cout << temp->sym;
-        temp = temp->next;
+        cout << current->sym;
+        current = current->next;
     }
     cout << endl;
 }
@@ -83,6 +111,7 @@ int main()
     int your_number;
     cout << "Enter a number: ";
     cin >> your_number;
+    cin.ignore(); // Игнорируем символ новой строки после ввода числа
 
     cout << "Enter characters (enter '.' to complete): " << endl;
 
@@ -94,7 +123,8 @@ int main()
         add_node(head, input, last_node);
     }
 
-    cout << "List of symbols: ";
-    exclude_words_by_char_code_sum(head);
+    exclude_words_by_char_code_sum(head, your_number);
+
+    cout << "After editing:\n";
     print_list(head);
 }
